d71bb6dd3254f11ddfcdc2c8a1b1184e587c16cf
Michael_Coulter <coulte12@go.stockton.edu>
2025-02-04 12:01:52 -0500

    complete

--- 2025 Spring/Foundations_of_Computer_Science/Lab1.py
+++ 2025 Spring/Foundations_of_Computer_Science/Lab1.py
@@ -1,4 +1,10 @@
 
+def main():
+    printTableFunction1()
+    printTableFunction2()
+    #printWholeTable()
+    #printTest()
+######################
 def Conjunction(p, q):
     return p and q
 
@@ -6,39 +12,55 @@ def Disjunction(p, q):
     return p or q
 
 def Conditional(p, q):
-    return not p or q
+    return (not p or q)
 
 def Biconditional(p, q):
     return (not p or q) and (not q or p)
 
+def Negation(p):
+    return not p
+######################
+def printTableFunction1():
+    printSingleHeader("Conditional [not p -> q]")
+    for p in [True, False]:
+        for q in [True, False]:
+            print("|\t" + str(p) + "\t|\t" + str(q) + "\t|\t" + str(Conditional(not p, q)) + "\t\t\t|")
+            printFrame()
+                
+def printTableFunction2():
+    printSingleHeader("Disjunction [p or q]")
+    for p in [True, False]:
+        for q in [True, False]:
+            print("|\t" + str(p) + "\t|\t" + str(q) + "\t|\t" + str(Disjunction(p, q)) + "\t\t\t|")
+            printFrame()
+######################
+def printWholeTable():
+    printWholeHeader()
+    for p in [True, False]:
+        for q in [True, False]:
+            print("|\t" + str(p) + "\t|\t" + str(q) + "\t|\t" + str(Conjunction(p, q)) + "\t\t|\t" + str(Disjunction(p, q)) + "\t\t|\t" + str(Conditional(p, q)) + "\t\t|\t" + str(Biconditional(p, q)) + "\t\t|\t" + str(Negation(p)) + "\t\t|")
+            printFrame()
 
-def main():
-    print("Conjunction")
-    print(Conjunction(True, True))
-    print(Conjunction(True, False))
-    print(Conjunction(False, True))
-    print(Conjunction(False, False))
-    print("______________________________________")
-
-    print("Disjunction")
-    print(Disjunction(True, True))
-    print(Disjunction(True, False))
-    print(Disjunction(False, True))
-    print(Disjunction(False, False))
-    print("_______________________________________")
-
-    print("Conditional")
-    print(Conditional(True, True))
-    print(Conditional(True, False))
-    print(Conditional(False, True))
-    print(Conditional(False, False))
-    print("________________________________________")
-
-    print("Biconditional")
-    print(Biconditional(True, True))
-    print(Biconditional(True, False))
-    print(Biconditional(False, True))
-    print(Biconditional(False, False))
-    print("_________________________________________")
+##################
+def printSingleHeader(s):
+    printFrame()
+    print("|\tp\t|\tq\t|\t%s" % s)
+    printFrame()
     
+def printDisjunctionHeader():
+    printFrame()
+    print("|\tp\t|\tq\t|\tDisjunction\t|")
+    printFrame()
+    
+def printWholeHeader():
+    printFrame()
+    print("|\tp\t|\tq\t|\tConjunction\t|\tDisjunction\t|\tConditional\t|\tBiconditional\t|\tNegation\t|")
+    printFrame()
+    
+def printFrame():
+    print("-" * 65)
+
+def printTest():
+    printFrame()
+    print("|%s" + str(Conjunction(True, False, True)) + "\t|")
 main()
